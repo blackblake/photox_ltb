fcbc2c969afb92b94786dcb272a183ed714ab0ba
floyd <blackblakebl@gmail.com>
2025-04-24 02:37:49 +0800

    初步完成了用户认证部分：注册、登录、获取用户信息

--- image_repo_backend/urls.py
+++ image_repo_backend/urls.py
@@ -1,22 +1,47 @@
-"""
-URL configuration for image_repo_backend project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+# image_repo_backend/urls.py
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+# 用于开发环境处理媒体文件访问
+from django.conf import settings
+from django.conf.urls.static import static
+
+# --- Swagger ---
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Photox API",
+        default_version='v1',
+        description="Photox 项目 API 文档",
+        # terms_of_service="https://www.google.com/policies/terms/",
+        # contact=openapi.Contact(email="contact@example.com"),
+        # license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,), # 允许任何人访问 API 文档
+)
+# --- Swagger End ---
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    # 包含 users 应用的 URL，并添加 'api/v1/auth/' 前缀
+    path('api/v1/auth/', include('users.urls', namespace='users')), # 使用 namespace='users'
+
+    # 你之后可以添加其他应用的 URL
+    # path('api/v1/images/', include('images.urls', namespace='images')),
+    # path('api/v1/albums/', include('albums.urls', namespace='albums')),
+    # path('api/v1/community/', include('community.urls', namespace='community')),
+
+    # Swagger API 文档路由
+    path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+
 ]
+
+# 开发环境下，允许 Django 托管用户上传的媒体文件
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- users/models.py
+++ users/models.py
@@ -1,1 +1,21 @@
 from django.db import models
+
+# users/models.py
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+class CustomUser(AbstractUser):
+    # 继承 AbstractUser，获得 username, password, email, first_name, last_name 等字段
+    # 添加自定义字段，参考 photox.md
+    avatar = models.ImageField(upload_to='avatars/', blank=True, null=True, verbose_name="头像")
+    bio = models.TextField(max_length=500, blank=True, null=True, verbose_name="简介")
+    # 根据需求添加更多字段，例如：
+    # birthday = models.DateField(null=True, blank=True)
+    # gender = models.CharField(max_length=1, choices=[('M', 'Male'), ('F', 'Female'), ('O', 'Other')], blank=True, null=True)
+
+    def __str__(self):
+        return self.username
+
+    class Meta:
+        verbose_name = "用户"
+        verbose_name_plural = verbose_name
