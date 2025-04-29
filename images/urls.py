7960ff6b8479bed935e8cd49e85b7e64f7c3c95f
x1x8j <1101219195@qq.com>
2025-04-29 10:01:51 +0800

    图片相关功能（初步）

---
+++ images/urls.py
@@ -0,0 +1,11 @@
+app_name = 'images'
+
+from django.urls import path
+from .views import ImageUploadView, ImageListView, ImageDetailView
+
+urlpatterns = [
+    path('upload/', ImageUploadView.as_view(), name='image-upload'),
+    path('', ImageListView.as_view(), name='image-list'),
+    path('<int:image_id>/', ImageDetailView.as_view(), name='image-detail-delete'),
+
+]
